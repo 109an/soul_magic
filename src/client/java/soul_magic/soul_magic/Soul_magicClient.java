da4a36c728cc7d664528e028997cd97c1533f773
109an <140023913+109an@users.noreply.github.com>
2024-02-16 08:41:29 -0500

    first commit

---
+++ src/client/java/soul_magic/soul_magic/Soul_magicClient.java
@@ -0,0 +1,10 @@
+package soul_magic.soul_magic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Soul_magicClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/soul_magic/soul_magic/Soul_magicDataGenerator.java
@@ -0,0 +1,11 @@
+package soul_magic.soul_magic;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Soul_magicDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
