889f45c0eda1a980943f0211efec69621ee5c0a3
109an <lgsky8@gmail.com>
2024-02-20 12:02:52 -0500

    item models for higher tier soulbottles

--- src/client/java/soul_magic/soul_magic/soul_magicClient.java
+++ src/client/java/soul_magic/soul_magic/soul_magicClient.java
@@ -45,6 +45,78 @@ public class Soul_magicClient implements ClientModInitializer {
 			}
 			return 0.0f;
 		});
+		ModelPredicateProviderRegistry.register(Soul_magic.SOUL_BOTTLE_TIER2, new Identifier("fill"), (stack, world, entity, seed) -> {
+			if(stack.hasNbt()){
+				double fill=stack.getNbt().getDouble("fill");
+				if(fill<14.28571429 && fill>0){
+					return 0.1f;
+				}
+				if(fill>14.28571429 && fill<28.57142858){
+					return 0.2f;
+				}
+				if(fill>28.57142858 && fill<42.85714278){
+					return 0.3f;
+				}
+				if(fill>42.85714278 && fill<57.14285698){
+					return 0.4f;
+				}
+				if(fill>57.14285698 && fill<71.42857118){
+					return 0.5f;
+				}
+				if(fill>71.42857118 && fill<85.71428538){
+					return 0.6f;
+				}
+				if(fill>85.71428538 && fill<100){
+					return 0.7f;
+				}
+				if(fill==100){
+					return 0.8f;
+				}
+				if(fill>100){
+					return 0.9f;
+				}
+				if(fill>200){
+					return 1.0f;
+				}
+			}
+			return 0.0f;
+		});
+		ModelPredicateProviderRegistry.register(Soul_magic.SOUL_BOTTLE_TIER3, new Identifier("fill"), (stack, world, entity, seed) -> {
+			if(stack.hasNbt()){
+				double fill=stack.getNbt().getDouble("fill");
+				if(fill<14.28571429 && fill>0){
+					return 0.1f;
+				}
+				if(fill>14.28571429 && fill<28.57142858){
+					return 0.2f;
+				}
+				if(fill>28.57142858 && fill<42.85714278){
+					return 0.3f;
+				}
+				if(fill>42.85714278 && fill<57.14285698){
+					return 0.4f;
+				}
+				if(fill>57.14285698 && fill<71.42857118){
+					return 0.5f;
+				}
+				if(fill>71.42857118 && fill<85.71428538){
+					return 0.6f;
+				}
+				if(fill>85.71428538 && fill<100){
+					return 0.7f;
+				}
+				if(fill==100){
+					return 0.8f;
+				}
+				if(fill>100){
+					return 0.9f;
+				}
+				if(fill>200){
+					return 1.0f;
+				}
+			}
+			return 0.0f;
+		});
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
 }
