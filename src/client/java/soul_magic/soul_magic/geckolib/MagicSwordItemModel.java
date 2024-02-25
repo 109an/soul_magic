ec9c6444ab77f7435b3bec5c2a33104f9804b685
109an <lgsky8@gmail.com>
2024-02-25 18:57:25 -0500

    fixed incorrect file paths

--- src/client/java/soul_magic/soul_magic/geckolib/MagicSwordItemModel.java
+++ src/client/java/soul_magic/soul_magic/geckolib/MagicSwordItemModel.java
@@ -12,11 +12,11 @@ public class MagicSwordItemModel extends GeoModel<MagicSwordItem> {
 
     @Override
     public Identifier getTextureResource(MagicSwordItem animatable) {
-        return new Identifier("soul_magic", "textures/item/magic_sword.png");
+        return new Identifier("soul_magic", "textures/item/magic_sword_item.png");
     }
 
     @Override
     public Identifier getAnimationResource(MagicSwordItem animatable) {
-        return new Identifier("soul_magic", "animations/magic_sword.animation.json");
+        return new Identifier("soul_magic", "animations/magic_sword_item.animation.json");
     }
 }
